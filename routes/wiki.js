e1d1a39042dcb4397130ac170698b431a5c59b22
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2020-02-09 15:56:36 -0600

    redirect to the newly added page via slug

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const addPage = require('../views/addPage');
+const wikiPage = require('../views/wikipage');
 const { Page } = require('../models');
 
 router.get('/', (req, res) => {
-    res.send('here\'s the wiki page');
-})
+  res.send("here's the wiki page");
+});
 
 router.post('/', async (req, res, next) => {
-    const page = new Page({
-        title: req.body.title,
-        content: req.body.content
-    });
-    try {
-        await page.save();
-        res.redirect('/');
-    } catch (error) {
-        next(error);
-    }
-    console.log(page);
-})
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
+    await page.save();
+    res.redirect(`/wiki/${page.slug}`);
+  } catch (error) {
+    next(error);
+  }
+  console.log(page);
+});
 
 router.get('/add', (req, res) => {
-    res.send(addPage());
-})
+  res.send(addPage());
+});
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const pageData = await Page.findOne({
+      where: { slug: req.params.slug },
+    });
+    res.send(wikiPage(pageData));
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
